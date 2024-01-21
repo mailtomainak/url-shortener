77a979e0c4f3d3c60d3e0cd55eb79245339cb76a
m5c <mailtomainak@gmail.com>
2024-01-21 12:03:20 +0100

    after rebasing

--- UrlShortener.Cli/obj/Debug/net8.0/UrlShortener.Cli.AssemblyInfo.cs
+++ UrlShortener.Cli/obj/Debug/net8.0/UrlShortener.Cli.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UrlShortener.Cli")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bbfdee08a05f4642255dd584e600ea2405a41ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("UrlShortener.Cli")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UrlShortener.Cli")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
